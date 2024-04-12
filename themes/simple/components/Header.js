077ded744734331683223e9f946d3e32c903a44b
951018066@qq.com <951018066@qq.com>
2024-04-12 14:59:51 +0800

    init

--- themes/simple/components/Header.js
+++ themes/simple/components/Header.js
@@ -23,8 +23,7 @@ export default function Header (props) {
                         </div>
 
                         <div className='flex-col flex justify-center'>
-                            <div className='text-2xl font-serif dark:text-white py-2 hover:scale-105 transform duration-200'>{siteConfig('AUTHOR')}</div>
-                            <DarkModeButton className='text-center pt-4'/>
+                            <div className='text-2xl font-serif dark:text-white py-2 hover:scale-105 transform duration-200'>{siteConfig('AUTHOR')}<DarkModeButton className='text-center pt-4'/></div>
                             <div className='font-light dark:text-white py-2 hover:scale-105 transform duration-200 text-center' dangerouslySetInnerHTML={{ __html: siteConfig('SIMPLE_LOGO_DESCRIPTION', null, CONFIG) }} />
                         </div>
                     </div>
