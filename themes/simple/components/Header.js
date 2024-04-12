a4aadf8ae8688b12ce21fce61e1b5b5454f2cc82
951018066@qq.com <951018066@qq.com>
2024-04-12 14:56:07 +0800

    init

--- themes/simple/components/Header.js
+++ themes/simple/components/Header.js
@@ -20,11 +20,11 @@ export default function Header (props) {
                     <div className='flex space-x-6'>
                         <div className='hover:rotate-45 hover:scale-125 transform duration-200 cursor-pointer justify-center items-center flex'>
                             <LazyImage priority={true} src={siteInfo?.icon} className='rounded-full' width={100} height={100} alt={siteConfig('AUTHOR')} />
-                            <DarkModeButton className='text-center pt-4'/>
                         </div>
 
                         <div className='flex-col flex justify-center'>
                             <div className='text-2xl font-serif dark:text-white py-2 hover:scale-105 transform duration-200'>{siteConfig('AUTHOR')}</div>
+                            <DarkModeButton className='text-center pt-4'/>
                             <div className='font-light dark:text-white py-2 hover:scale-105 transform duration-200 text-center' dangerouslySetInnerHTML={{ __html: siteConfig('SIMPLE_LOGO_DESCRIPTION', null, CONFIG) }} />
                         </div>
                     </div>
